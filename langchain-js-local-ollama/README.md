2ebd19c171c42bad8685e3cccbf3ccde2df4ce41
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-11-10 15:00:41 -0500

    Summary: Ready for round 2

--- langchain-js-local-ollama/README.md
+++ langchain-js-local-ollama/README.md
@@ -4,6 +4,13 @@ See <https://js.langchain.com/docs/use_cases/question_answering/local_retrieval_
 
 > For example, here we show how to run Llama 2 locally (e.g., on your laptop) using local embeddings, a local vector store, and a local LLM. You can check out other open-source models supported by Ollama here.
 
+## Results
+
+First summarization results:
+
+- llama2: 38 minutes -redo with recursive tokenizer
+- mistral: 24 minutes
+
 ## Operation
 
 Before you invoke the `index.mjs` script, you need to start the `ollama` server.
--- langchain-js-local-ollama/summarize.mjs
+++ langchain-js-local-ollama/summarize.mjs
@@ -5,7 +5,8 @@ import {
   getSourceForText,
 } from "./sources.mjs";
 
-import { TokenTextSplitter } from "langchain/text_splitter";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
 import { Ollama } from "langchain/llms/ollama";
 import { PromptTemplate } from "langchain/prompts";
 import { loadSummarizationChain } from "langchain/chains";
@@ -15,18 +16,17 @@ const { name, question, loader } = await getSourceForEPub();
 // const { name, question, loader } = await getSourceForText();
 // const { name, question, loader } = await getSourceForBlog();
 
-console.log(`## 1- Fetch/spit document (${name})\n`);
+console.log(`\n## 1- Fetch/split document (${name})\n`);
 
 const docs = await loader.load();
 
-const splitter = new TokenTextSplitter({
-  chunkSize: 1000,
-  chunkOverlap: 50,
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 4000,
+  chunkOverlap: 200,
 });
 
 const splitDocuments = await splitter.splitDocuments(docs);
 console.log(`  - split into ${splitDocuments.length} document chunks`);
-
 // console.log(JSON.stringify(splitDocuments, null, 2));
 
 const summaryTemplate = `
@@ -75,8 +75,8 @@ const SUMMARY_REFINE_PROMPT = PromptTemplate.fromTemplate(
 // Llama 2 7b wrapped by Ollama
 const model = new Ollama({
   baseUrl: "http://localhost:11434",
-  // model: "llama2",
-  model: "mistral",
+  model: "llama2",
+  // model: "mistral",
 });
 
 const summarizeChain = loadSummarizationChain(model, {
@@ -86,9 +86,9 @@ const summarizeChain = loadSummarizationChain(model, {
   refinePrompt: SUMMARY_REFINE_PROMPT,
 });
 
-console.log(`## 2- Summary Chain (${name})\n`);
+console.log(`\n## 2- Summary Chain (${name})\n`);
 const summary = await summarizeChain.run(splitDocuments);
 
-console.log(`## 3- FFinal Summary and Questions for ${name}\n`);
+console.log(`\n## 3- Final Summary and Questions for ${name}\n`);
 
 console.log(summary);
