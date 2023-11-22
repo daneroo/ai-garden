42d1f7e6a29a9dde387e7a27e93b3b637886652c
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-11-22 17:40:16 -0500

    TODO: tokens

---
+++ langchain-js-local-ollama/experiments/basics-tokens.mjs
@@ -0,0 +1,6 @@
+console.log(`# Basics Tokens
+
+**TODO**: implement this
+
+Here we simply tokenize a source, and try to determine what the maximum number of tokens we can pass to an llm instance
+\n`);
