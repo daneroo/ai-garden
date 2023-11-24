d003e2f57e4c45c9a83ab29c721ba80c2eaf9e20
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-11-23 19:14:49 -0500

    Working zod shcema for characters and colors.

--- langchain-js-local-ollama/experiments/basics-output-parser-complex.mjs
+++ langchain-js-local-ollama/experiments/basics-output-parser-complex.mjs
@@ -10,19 +10,28 @@ Here we simply invoke a chain (RunnableSequence)
 that includes an StructuredOutputParser (Zod) as it's last step.
 
 We are parsing an output that is a list of colors and moods.
-The output is a JSON Object, so we use Zod to parse it.
+The output is a JSON Object, so we use Zod to specify the schema.
 
-Mistral seems much better at this than Llama2.
+Mistral seemed much better at this than Llama2.
+But after tuning prompt and schema, Llama2 is better.
+And now both are pretty reliable.
+Temperature of the model was set to 0, which helped a lot.
+
 
 - Five colors/moods
-  - Model: llama2 - Total attempts: 52 Success rate: 19.23%
-  - Model: llama2 - Total attempts: 39 Success rate: 25.64%
-  - Model: mistral - Total attempts: 17 Success rate: 58.82%
-  - Model: mistral - Total attempts: 11 Success rate: 90.91%
+  - Model: mistral - Total attempts: 10 Success rate: 100.00%
+  - Model: mistral - Total attempts: 10 Success rate: 100.00%
+  - Model: llama2 - Total attempts: 10 Success rate: 100.00%
+  - Model: llama2 - Total attempts: 10 Success rate: 100.00%
+- Novel characters
+  - Model: mistral - Total attempts: 10 Success rate: 100.00%
+  - Model: mistral - Total attempts: 10 Success rate: 100.00%
+  - Model: llama2 - Total attempts: 10 Success rate: 100.00%
+  - Model: llama2 - Total attempts: 10 Success rate: 100.00%
 \n`);
 
 const verbose = false;
-const modelName = "mistral"; // llama2,mistral
+const modelName = "llama2"; // llama2,mistral
 
 const model = new ChatOllama({
   baseUrl: "http://localhost:11434",
@@ -30,31 +39,75 @@ const model = new ChatOllama({
   maxConcurrency: 1,
   // actually makes things worse??
   // format: "json",
+  temperature: 0, // very helpful for this task
 });
 
 console.log(`## Chat modelName: ${modelName}\n`);
 // console.log(`- Model: ${JSON.stringify(model, null, 2)}`);
 
-const parser = StructuredOutputParser.fromZodSchema(
-  z
-    .array(
-      z.object({
-        color: z.string().describe("the name of the color"),
-        mood: z.string().describe("The mood associated with the color"),
+const schemaAndTemplateFString = {
+  colorsAndMoods: {
+    schema: z
+      .object({
+        colors: z.array(
+          z.object({
+            name: z.string().describe("the name of the color"),
+            mood: z.string().describe("The mood associated with the color"),
+          })
+        ),
       })
-    )
-    .describe("A list of colors and moods")
-);
+      .describe("A list of colors and moods"),
+    templateFString: `
+    Make a list of 3 colors.
+    For each color, specify the name of the color and a mood associated with the color
+    
+    Format the output as JSON. 
+    {format_instructions}
+    
+    Only include the JSON Output. No other text is allowed.
+    `,
+  },
+  novelCharacters: {
+    schema: z
+      .object({
+        characters: z.array(
+          z.object({
+            name: z.string().describe("name of the character"),
+            // gender: z.enum(["man", "woman"]).describe("gender of the character"),
+            description: z
+              .string()
+              .describe("short description of the character"),
+          })
+        ),
+      })
+      .describe("A list of characters"),
+    templateFString: `
+    - Pick a well known novel. 
+    - Name 3 main characters in the novel.
+    - Only show the list of characters.
+    - For each character, specify their name and a very short description.
+    
+    Format the output as JSON.
+    {format_instructions}
+    
+    `,
+  },
+};
+
+// which schema and template to use
+// const { schema, templateFString } = schemaAndTemplateFString.colorsAndMoods;
+const { schema, templateFString } = schemaAndTemplateFString.novelCharacters;
+
+const parser = StructuredOutputParser.fromZodSchema(schema);
 
 if (verbose) {
   console.log("Parser instructions:\n", parser.getFormatInstructions());
 }
-const promptTemplate = PromptTemplate.fromTemplate(`
-Make a list of 5 colors and an associated mood for each.
-{format_instructions}
-Make a list of 5 colors and an associated mood for each.`);
+
+const promptTemplate = PromptTemplate.fromTemplate(templateFString);
 
 const chain = RunnableSequence.from([promptTemplate, model, parser]);
+// const chain = RunnableSequence.from([promptTemplate, model]);
 
 if (verbose) {
   console.log(`- Chain: ${JSON.stringify(chain, null, 2)}`);
@@ -70,12 +123,13 @@ async function untilSuccessful() {
         // topic,
         format_instructions: parser.getFormatInstructions(),
       });
+
       if (verbose) {
         console.log(`- Result: ${JSON.stringify(result, null, 2)}\n`);
       }
       return { result, attempts };
     } catch (err) {
-      // console.error(err);
+      console.error(err);
       console.log(`  .. Failed to parse text. Retrying...`);
     }
   }
@@ -89,7 +143,7 @@ for (let i = 0; i < 10; i++) {
   totalSuccesses += 1;
   const successRate = ((1.0 / attempts) * 100).toFixed(2);
   console.log(
-    `- #${i} success rate: ${successRate}% attempts: ${attempts}, list of colors and moods as a JSON Object:\n`,
+    `- #${i} success rate: ${successRate}% attempts: ${attempts}, result as a JSON Object:\n`,
     JSON.stringify(result)
   );
 }
