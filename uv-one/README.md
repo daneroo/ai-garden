cd7b97ebb94f0e37c8e8fc07cacd668213ab86de
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-10-11 18:35:10 -0400

    First project with UV!!!

---
+++ uv-one/README.md
@@ -0,0 +1,11 @@
+# uv & ruff
+
+Just trying to get rid of poetry
+
+```bash
+uv init uv-one
+cd uv-one
+uv run hello.py 
+uv add requests
+uv run hello.py
+```
---
+++ uv-one/hello.py
@@ -0,0 +1,11 @@
+import requests
+
+def main():
+    print("Hello from uv-one!")
+
+    response = requests.get("https://api.github.com")
+    print(f"GitHub API status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
